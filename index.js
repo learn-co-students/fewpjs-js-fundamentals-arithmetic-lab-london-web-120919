237344157aa7c866881083b159750d75833b2560
sarafotros <sarafotros@gmail.com>
2020-05-14 19:58:44 +0100

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+
+// const multiply = 31 * 2 ;
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const mod = 14 % 5;
+
+const max = Math.max(1,2,3,4,20,8,5,11)
